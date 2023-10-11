098e1d934e9e2f62d38422358ba8b1d68ef9ae80
leeyongseok13 <minamiharuka333@gmail.com>
2023-10-11 17:53:44 +0900

    20231011

--- js/main.js
+++ js/main.js
@@ -3,5 +3,16 @@ $(function () {
     sectionsColor: ["#ffffff", "#f9f9f9", "#ffffff", "#f9f9f9", "#ffffff"],
     loopTop: false,
     loopBottom: false,
+    navigation: true,
+    onLeave: function (origin, destination, direction, trigger) {
+      var origin = this;
+
+      if (origin.index == 0 && direction == "down") {
+        document.querySelector("#header").style.color = "#000000";
+        document.querySelector("#fp-nav").style.color = "000000";
+      } else if (origin.index == 1 && direction == "up") {
+        document.querySelector("#header").style.color = "#f9f9f9";
+      }
+    },
   });
 });
